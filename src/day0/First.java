7a348cb6ac369d008baa0c7c98a48c5cbec16c06
yprkvrr <98862576+yprkvrr@users.noreply.github.com>
2022-03-30 10:27:49 -0400

    updated first file too

--- src/day0/First.java
+++ src/day0/First.java
@@ -1,6 +1,11 @@
 package day0;
 
 public class First {
-    //TODO:Works on this tomporrow
+    //TODO:Works on this tomorrow
     // add some cool code
+    //extra line 1
+    public static void main(String[] args) {
+        System.out.println("java");
+        System.out.println("working....");
+    }
 }
